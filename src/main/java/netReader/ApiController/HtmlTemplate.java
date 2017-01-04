f3f695b775e62c2f994c683be8f8cadacd3d9618
Nettle <nettle256@gmail.com>
2017-01-04 18:55:50 +0800

    更新模板获取的API

--- src/main/java/netReader/ApiController/HtmlTemplate.java
+++ src/main/java/netReader/ApiController/HtmlTemplate.java
@@ -1,18 +1,23 @@
 package netReader.ApiController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Nettle on 2017/1/4.
  */
 @Controller
-@RequestMapping(path="/template")
+@RequestMapping(path="/html")
 public class HtmlTemplate {
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String getHtmlTemplate(@RequestParam(required = true, value = "uri") String uri) {
-        return "ng-template/"+uri;
+    @RequestMapping(value = "/**", method = RequestMethod.GET)
+    public String getHtmlTemplate(HttpServletRequest request) {
+        String pattern = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        String path = new AntPathMatcher().extractPathWithinPattern(pattern, request.getServletPath());
+        return path;
     }
 }
